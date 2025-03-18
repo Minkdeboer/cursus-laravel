442cd6a540becc1fd012546792fb5c794e3bd7cb
Minkdeboer <114353459+Minkdeboer@users.noreply.github.com>
2025-03-18 12:36:34 +0100

    Section 16: Query Builder Joins
    88. 1_Joins - Introduction
    
    89. 2_Joins - Making Database Ready
    
    90. 3_Joins - Inner Join
    
    91. 4_Joins - Left Join
    
    92. 5_Joins - Right Join
    
    93. 6_Joins - Full Join

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,31 @@ Route::delete('customers/trash/{customer}', [CustomerController::class, 'forceDe
 Route::resource('customers', CustomerController::class)->names([
     'create' => 'customer.create'
 ]);
+
+Route::get('/join', function(){
+    //   $usersWithOrders = DB::table('users')
+    //   ->join('orders', 'users.id', '=', 'orders.user_id')
+    //   ->select('users.*', 'orders.*')
+    //   ->get();
+
+    // $usersWithOrders = DB::table('users')
+    // ->leftJoin('orders', 'users.id', '=', 'orders.user_id')
+    // ->select('users.name', 'orders.id as order_id') // Assuming 'id' is a column in 'orders' table
+    // ->get();
+
+    // $ordersWithUsers = DB::table('orders')
+    // ->rightJoin('users', 'users.id', '=', 'orders.user_id')
+    // ->select('orders.product_name', 'users.name')
+    // ->get();
+
+    $fullOuterJoin = DB::table('users')
+    ->leftJoin('orders', 'users.id', '=', 'orders.user_id')
+    ->select('users.name', 'orders.product_name')
+    ->unionAll(
+        DB::table('users')
+        ->rightJoin('orders', 'users.id', '=', 'orders.user_id')
+        ->select('users.name', 'orders.product_name')
+    );
+
+      dd($fullOuterJoin);
+});
